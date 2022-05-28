f0b0f449060a7ef030f1981e228ed28c1e00c8df
j0code <jonaslp759@gmail.com>
2022-05-28 21:18:39 +0200

    partial dm msgs, dm reply

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ import { Client } from "discord.js";
 import config from "./config.json" assert {type: "json"};
 import { load, register } from "./commands.js"
 
-export const bot = new Client({ intents: ["GUILDS", "GUILD_MESSAGES", "GUILD_VOICE_STATES"]});
+export const bot = new Client({ intents: ["GUILDS", "GUILD_MESSAGES", "GUILD_VOICE_STATES", "DIRECT_MESSAGES"], partials: ["MESSAGE","CHANNEL"] });
 
 bot.on("ready", async () => {
   console.log(`Logged in as ${bot.user.tag}!`);
@@ -17,10 +17,29 @@ bot.on("ready", async () => {
 
   await load(bot);
   register(bot);
+
+	/*bot.users.fetch("418109742183874560").then(u => {
+		console.log(u)
+		u.send("hi")
+	})*/
 })
 
 bot.on("messageCreate", (msg) => {
   // TODO: handle guesses
+	console.log(msg.author.tag, msg.content)
+	if(msg.author.id == bot.user.id) return
+	if(msg.partial) return msg.fetch().then(handleMessage)
+	else handleMessage(msg)
 })
+
+function handleMessage(msg) {
+	if(!msg.guild) {
+		msg.channel.send(`I can't help you with that. I'm just a bot. Maybe ask a human.
+
+I can only recommend @Certified Financial Planner and @j0code.
+Also try talking to people in discord.gg/Uf8nbhh`)
+	return
+	}
+}
 
 bot.login(config.token);
